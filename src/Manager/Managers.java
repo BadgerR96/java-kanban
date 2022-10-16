58d68863bf9c14197652a49a94ab9e496d8a5bf1
Badger96 <badger96rus@yandex.ru>
2022-10-16 19:34:14 +0500

    Финальное задание 4

--- src/Manager/Managers.java
+++ src/Manager/Managers.java
@@ -5,13 +5,10 @@ import Model.Task;
 import java.util.List;
 
 public class Managers {
-    private TaskManager taskManager = new InMemoryTaskManager();
-    private static HistoryManager historyManager = new InMemoryHistoryManager();
-
-    public TaskManager getDefault(){
-        return taskManager;
+     public static TaskManager getDefault(){
+        return new InMemoryTaskManager();
     }
     public static HistoryManager getDefaultHistory(){
-        return historyManager;
+        return new InMemoryHistoryManager();
     }
 }
